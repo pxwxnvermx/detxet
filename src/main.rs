10de5fe2b8f664b3be7611c0daac477c77734021
pxwxnvermx <pawanjbs5@gmail.com>
2024-01-23 19:39:05 +0530

    Added Editor state

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,61 @@
-use std::io::{stdout, Stdout, Write};
+use std::{
+    env,
+    io::{stdout, Stdout, Write},
+};
 
 use crossterm::{
-    cursor::MoveTo,
+    cursor::{MoveLeft, MoveRight, MoveTo},
     event::{self, Event, KeyCode, KeyEvent},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
 
-fn flush_content_to_terminal(stdout: &mut Stdout, content: &String) {
-    execute!(stdout, Clear(ClearType::All)).expect("failed clearing");
-    execute!(stdout, MoveTo(0, 0)).expect("error moving");
-    write!(stdout, "{}", content).expect("failed writing");
-    stdout.flush().expect("failed in startup");
+struct Editor {
+    stdout: Stdout,
+    content: String,
+    cursor_pos: (u16, u16),
 }
 
-fn some_fn(some: &mut String) {
-    print!("{some}");
+trait EditorCommands {
+    fn flush_content_to_terminal(&mut self);
+    fn clear_screen(&mut self);
+    fn move_cursor(&mut self);
 }
+
+impl EditorCommands for Editor {
+    fn flush_content_to_terminal(&mut self) {
+        self.clear_screen();
+        write!(self.stdout, "{}", self.content).expect("failed writing");
+        self.move_cursor();
+        self.stdout.flush().expect("failed in startup");
+    }
+
+    fn clear_screen(&mut self) {
+        execute!(self.stdout, Clear(ClearType::All)).expect("failed clearing");
+    }
+
+    fn move_cursor(&mut self) {
+        execute!(self.stdout, MoveTo(self.cursor_pos.0, self.cursor_pos.1)).expect("error moving");
+    }
+}
+
+/*
+* TODO: Opening files
+* TODO: Saving a file
+* */
 
 fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
     enable_raw_mode().expect("failed enable raw mode");
+    let mut editor = Editor {
+        stdout: stdout(),
+        content: String::new(),
+        cursor_pos: (0, 0),
+    };
 
-    let stdout = stdout();
-    let mut content = String::new();
-    execute!(&stdout, Clear(ClearType::All)).expect("failed clearing");
-    execute!(&stdout, MoveTo(0, 0)).expect("error moving");
-
-    let some_s = String::new();
-    some_fn(&mut some_s);
-
+    editor.clear_screen();
+    // event loop
     loop {
         if let Event::Key(event) = event::read()? {
             match event {
@@ -41,31 +68,47 @@ fn main() -> std::io::Result<()> {
                     code: KeyCode::Char(c),
                     ..
                 } => {
-                    content.push(c);
+                    editor.content.push(c);
+                    editor.cursor_pos.0 += 1;
                 }
                 KeyEvent {
                     code: KeyCode::Backspace,
                     ..
                 } => {
-                    content.pop();
+                    editor.content.pop();
+                    editor.cursor_pos.0 -= 1;
                 }
                 KeyEvent {
                     code: KeyCode::Enter,
                     ..
                 } => {
-                    content += "\r\n";
+                    editor.content.push_str("\r\n");
+                    editor.cursor_pos.1 += 1;
                 }
                 KeyEvent {
                     code: KeyCode::Tab, ..
                 } => {
-                    content += "\t";
+                    editor.content.push_str("\t");
+                }
+                KeyEvent {
+                    code: KeyCode::Left,
+                    ..
+                } => {
+                    editor.cursor_pos.0 -= 1;
+                    execute!(editor.stdout, MoveLeft(1)).expect("failed moving left");
+                }
+                KeyEvent {
+                    code: KeyCode::Right,
+                    ..
+                } => {
+                    editor.cursor_pos.0 += 1;
+                    execute!(editor.stdout, MoveRight(1)).expect("failed moving right");
                 }
                 _ => {
                     //todo
                 }
             }
-
-            flush_content_to_terminal(&stdout, &content)
+            editor.flush_content_to_terminal()
         };
     }
 
